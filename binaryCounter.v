c7c639d88c3c97e949fe54b123e073a273fbaa7d
AbdoAhmed24 <21p0333@eng.asu.edu.eg>
2023-12-20 18:20:12 +0200

    NewModule

--- binaryCounter.v
+++ binaryCounter.v
@@ -1,10 +1,16 @@
 module BinaryCounter(
-    input clk,   // Clock input
-    input [2:0] rst,   // Reset input
+    input clk,         // Clock input
+    input [2:0] rst,    // Reset input
     input x,
     input y,
+    input a, 
+    input c,
+    input d,
+    input g,
+    input f,	
     input wire [31:0] b,
-    output reg [5:0] count  // 4-bit binary counter output
+    output reg [5:0] count,  // 4-bit binary counter output
+    output wire result     // Output based on continuous assignment
 );
 
     // Define a 4-bit register to hold the count
@@ -14,6 +20,12 @@ module BinaryCounter(
             count <= 4'b0000;
         else      // Increment on each rising edge of the clock
             count <= count + 1;
-    ends
+    end
+
+    // Continuous assignment for 'result' based on inputs x and y
+    assign result = x ^ y;
+    assign result = (a & c) ? (f + 4'b0001) : (g - 4'b0010);
 
 endmodule
+
+ 
